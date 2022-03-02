cef7a61a167af12925542219d9c9a3b6b340632e
JamesH <jwhusc7@gmail.com>
2022-03-02 17:21:53 -0500

    added quiz functionality with feedback

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,107 @@
-var startButtonEl = document.querySelector("button[value='start']");
-var questionSectionEl = document.getElementById("question");
-var score = 0;
-var questions = [
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+const questionContainerEl = document.getElementById('question-container');
+const questionEl = document.getElementById('question')
+const answerButtonsEl = document.getElementById('answer-buttons');
+
+let shuffledQuestions, currentQuestionIndex;
+
+const questions = [
     {
-        question: "Which of the following is a primitive DataType",
-        answers: ["array", "function", "integers", "object"],
-        correctIndex: 2
+        question: "which datatype is primitive?",
+        answers: [
+            {text: 'boolean', correct: true},
+            {text: 'array', correct: false}
+        ]
+    },
+    {
+        question: "which is not a falsy value?",
+        answers: [
+            {text: '0', correct: false},
+            {text: '"nothing"', correct: true}
+        ]
+    },
+    {
+        question: "A function passed as an argument to another function is called what?",
+        answers: [
+            {text: 'a function sequence', correct: false},
+            {text: 'a callback function', correct: true}
+        ]
     }
 ];
 
+function clearFeedback(element) {
+    element.classList.remove('correct')
+    element.classList.remove('incorrect')
+};
 
-function runQuiz() {
-    startButtonEl.value, startButtonEl.innerHTML = "submit"
-    // loop through questions array, display question 
-    // until an answer is clicked, if right then score++
-    for (var i = 0; i < questions.length; i++){
-        questionSectionEl.innerHTML = questions[i].question;
-        // loop length of answers displaying each
-        // click event for each, data-id for 
+function resetState() {
+    nextButton.classList.add('hide');
+    while (answerButtonsEl.firstChild) {
+        answerButtonsEl.removeChild(answerButtonsEl.firstChild)
+    }
+};
+
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+};
+
+function displayFeedback(element, correct){
+    let feedbackP = document.createElement('p')
+    feedbackP.className = "feedback"
+    feedbackP.id = "feedback"
+    clearFeedback(element)
+    if (correct) {
+        feedbackP.textContent =  "Correct"
+        questionContainerEl.appendChild(feedbackP)
+    } else {
+        feedbackP.textContent =  "Incorrect"
+        questionContainerEl.appendChild(feedbackP)
+    }
+};
+
+function answerClicked(event) {
+    const selectedButton = event.target;
+    const correct = selectedButton.dataset.correct;
+    displayFeedback(selectedButton, correct)
+
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide');
+    } else {
+        startButton.innerText = 'Restart';
+        startButton.classList.remove('hide');
     };
 };
 
-// start button on click populates 
-// question div with first question and answer choices
-startButtonEl.addEventListener("click", runQuiz);
+function showQuestion(question){
+    questionEl.innerText = question.question;
+
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct){
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', answerClicked)
+        answerButtonsEl.appendChild(button)
+    });
+};
+
+function startGame(){
+    console.log('started')
+    startButton.classList.add('hide');
+    shuffledQuestions= questions.sort(() => Math.random - .5); //make random work
+    currentQuestionIndex = 0;
+    questionContainerEl.classList.remove('hide');
+    setNextQuestion();
+};
+
+startButton.addEventListener('click', startGame);
+
+nextButton.addEventListener('click', () => {
+    document.getElementById('feedback').remove()
+    currentQuestionIndex++
+    setNextQuestion()
+});
